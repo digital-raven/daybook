bc647553a35168f44093b74d9e3f780c1a8260ff
Bo Cimino <ciminobo@protonmail.com>
2019-10-12 20:14:31 -0500

    03-package-for-debian
    Created a package.bash script which can create .deb, rpm,
    python3 wheel, and regular tar.gz package.
    
    requirements.txt is removed in favor of hardcoding within
    setup.py. Turns out setuptools is preferred over pip, so
    it wasn't needed.

--- setup.py
+++ setup.py
@@ -7,13 +7,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt'), 'r', encoding='utf-8') as f:
-    install_requires = []
-    for line in f.readlines():
-        commentless_line = line.split('#', 1)[0].strip()
-        if commentless_line:
-            install_requires.append(commentless_line)
-
 setup(
     name='daybook',
     version='0.1.0',
@@ -26,8 +19,14 @@ setup(
     packages=find_packages(exclude=['tests']),
     license='Closed',
     python_requires='>3, <4',
-    install_requires=install_requires,
-
+    install_requires=[
+        'dateparser >= 0.7.2, < 1.0',
+        'argcomplete >= 1.0',
+    ],
+    data_files=[
+        ('/etc/daybook', ['etc/default.ini']),
+        ('/etc/bash_completion.d', ['etc/daybook_completion.sh']),
+    ],
     entry_points={
         'console_scripts': [
             'daybook=daybook.client:main',
