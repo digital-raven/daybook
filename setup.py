6abb5ac46eaaddc48399f727eab6c89d5b7509b5
Bo Cimino <ciminobo@protonmail.com>
2023-01-03 22:40:29 -0600

    fix: Installation of data files when using pip

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ setup(
     python_requires='>3, <4',
     install_requires=install_requires,
     data_files=[
-        ('/etc/daybook', ['etc/default.ini']),
-        ('/etc/bash_completion.d', ['etc/daybook_completion.sh']),
-        ('/usr/share/man/man1', manfiles),
+        ('etc/daybook', ['etc/default.ini']),
+        ('etc/bash_completion.d', ['etc/daybook_completion.sh']),
+        ('usr/share/man/man1', manfiles),
     ],
     entry_points={
         'console_scripts': [
