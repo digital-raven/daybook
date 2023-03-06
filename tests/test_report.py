e683a13aa3e1ec1ce3de91f5fd77e2fabbe9bc54
Logan Reece <onereddime@protonmail.com>
2023-03-06 11:59:32 -0600

    Refactored importing logic
    report and convert now use a more general set of functions in
    daybook.util.importer

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,7 +1,8 @@
 import os
 import unittest
 
-from daybook.client.cli.report.main import import_reporter
+from daybook.client.cli.report.main import report_filter
+from daybook.util.importer import import_module
 
 
 resources = f'{os.path.dirname(__file__)}/resources/report'
@@ -11,22 +12,22 @@ class TestReport(unittest.TestCase):
     """ Test report subcommand's functions.
     """
     def test_import_reporter(self):
-        """ The same as import_single_py but check fields.
+        """ Valid report import.
         """
-        _, _, report, _ = import_reporter(f'{resources}/report.py')
+        report, = import_module(f'{resources}/report.py', report_filter, keys=['report'])
         self.assertEqual('some report', report(None, None))
 
     def test_import_reporter_doesnt_exist(self):
         """ Should raise OSError if pyfile doesn't exist.
         """
         with self.assertRaises(OSError):
-            _, _, _, _ = import_reporter(f'{resources}/doesnt_exist.py')
+            import_module(f'{resources}/doesnt_exist.py', report_filter)
 
     def test_import_reporter_missing_report(self):
         """ report function should be present.
         """
         with self.assertRaises(KeyError):
-            _, _, _, _ = import_reporter(f'{resources}/bad_report_missing_report.py')
+            import_module(f'{resources}/bad_report_missing_report.py', report_filter)
 
 
 if __name__ == '__main__':
